6104d82ec72778b64660e75fee1c440ca2201a0f
AbrahamMormontoy <aem19@sfu.ca>
2026-05-06 16:34:49 -0700

    Adding Links.jsx under tabs, shows my main information and its adaptative.

--- my_website/src/main.jsx
+++ my_website/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import About from './tabs/About.jsx'
+import Links from './tabs/Links.jsx'
 import { ThemeProvider } from './components/clouds/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <App/>
+      <About />
+      <Links />
     </ThemeProvider>
   </StrictMode>,
 )
